427a03288c6f24847f67e2988ce463398bd73537
SantiMinio <santiago.mino@davinci.edu.ar>
2020-12-03 18:45:07 -0300

    Más cositassss

---
+++ Assets/Scripts/Managers y Tools/ShutDownVersion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShutDownVersion : MonoBehaviour
+{
+    public const bool ShutDownActive = true;
+
+    enum PlatformObject { Mobile, PC }
+
+    [SerializeField] PlatformObject platformDisplay = PlatformObject.Mobile;
+
+    private void Awake()
+    {
+        if (!ShutDownActive) return;
+
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            if (platformDisplay == PlatformObject.Mobile) gameObject.SetActive(true);
+            else gameObject.SetActive(false);
+        }
+        else
+        {
+            if (platformDisplay == PlatformObject.Mobile) gameObject.SetActive(false);
+            else gameObject.SetActive(true);
+        }
+    }
+}
