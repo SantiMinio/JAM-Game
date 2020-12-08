cb7c16ade35b60d05a9e55a3c9064a4fd4600aed
Gonzalou <56851977+Gonzalou@users.noreply.github.com>
2020-12-08 19:57:43 -0300

    estoy aprendiendo todo de cero
    equide

--- Assets/Editor/LvlEditorWindow.cs
+++ Assets/Editor/LvlEditorWindow.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
+using System.IO;
+using System;
+using Object = UnityEngine.Object;
 
 public class LvlEditorWindow : EditorWindow
 {
+    public bool toogleButtons = true;
+    public Object LoadedLvlSet;
+
     public GUIStyle St;
+    private bool _hasLvlSet;
+    private List<GameObject> TilesList;
     public static void OpenWindow()
     {
         LvlEditorWindow instance = (LvlEditorWindow)GetWindow(typeof(LvlEditorWindow));
@@ -14,10 +23,52 @@ public class LvlEditorWindow : EditorWindow
     }
     private void OnGUI()
     {
-        if (GUILayout.Button("New Level Set",GUILayout.Width(150), GUILayout.ExpandWidth(false), GUILayout.Height(75)))
+        if (toogleButtons)
         {
+            if (GUILayout.Button("New Level Set", GUILayout.Width(150), GUILayout.ExpandWidth(false), GUILayout.Height(75)))
+            {
+                if (GameObject.Find("LevelSet"))
+                {
+                    var w = CreateInstance<NewLevelAlert>();
+                    w.position = new Rect(Screen.width / 2, Screen.height / 2, 200, 100);
+                    w.ShowPopup();
+                    w.BlockWindow(this, null);
+                }
+                else
+                {
+                    GameObject prefab = PrefabUtility.LoadPrefabContents("Assets/Editor/LevelSet.prefab");
+                    var g = Instantiate(prefab, Vector3.zero, Quaternion.Euler(Vector3.zero));
+                    g.name = "LevelSet";
+                    Debug.Log("se creo un Level set");
+                }
+                _hasLvlSet = true;
+            }
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Load Level Set", GUILayout.Width(150), GUILayout.ExpandWidth(false), GUILayout.Height(75)))
+            {
+                if (LoadedLvlSet)
+                {
+                    var w = CreateInstance<NewLevelAlert>();
+                    w.position = new Rect(Screen.width / 2, Screen.height / 2, 200, 100);
+                    w.ShowPopup();
+                    w.BlockWindow(this, LoadedLvlSet);
+                }
+                else
+                {
+                    Debug.Log("Load Level field is empty");
+                }
+            }
+            LoadedLvlSet = EditorGUILayout.ObjectField("Level Set", LoadedLvlSet, typeof(object), true);
+            EditorGUILayout.EndHorizontal();
+            if (GUILayout.Button("Save Level Set", GUILayout.Width(150), GUILayout.ExpandWidth(false), GUILayout.Height(75)))
+            {
 
-        } 
+            }
+        }
+    }
+
+    void TilesOptions()
+    {
 
     }
 }
---
+++ Assets/Editor/NewLevelAlert.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class NewLevelAlert : EditorWindow
+{
+    private LvlEditorWindow Lvlwindow;
+    private GameObject currentLvl;
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("  A LevelSet already exists", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Do you want to overwrite it?", EditorStyles.boldLabel);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Yes", GUILayout.Height(30)))
+        {
+            var b = GameObject.Find("LevelSet");
+            DestroyImmediate(b);
+            if (currentLvl == null)
+            {
+                GameObject prefab = PrefabUtility.LoadPrefabContents("Assets/Editor/LevelSet.prefab");
+                var g = Instantiate(prefab, Vector3.zero, Quaternion.Euler(Vector3.zero));
+                g.name = "LevelSet";
+
+            }
+            else
+            {
+                Instantiate(currentLvl, Vector3.zero, Quaternion.Euler(Vector3.zero));
+            }
+            Lvlwindow.toogleButtons = true;
+            Close();
+        }
+        if (GUILayout.Button("No", GUILayout.Height(30)))
+        {
+            Lvlwindow.toogleButtons = true;
+            Close();
+        }
+    }
+    public void BlockWindow(LvlEditorWindow window, Object LoadedLvl)
+    {
+        window.toogleButtons = false;
+        Lvlwindow = window;
+    }
+
+
+}
